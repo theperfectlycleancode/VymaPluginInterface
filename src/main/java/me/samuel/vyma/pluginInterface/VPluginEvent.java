ece4bb5145d4ba4d1485ade23d0b39da0a439086
theperfectlycleancode <samgamesx.sam@gmail.com>
2019-04-28 23:46:55 +0200

    Interface.

---
+++ src/main/java/me/samuel/vyma/pluginInterface/VPluginEvent.java
@@ -0,0 +1,4 @@
+package me.samuel.vyma.pluginInterface;
+
+public abstract class VPluginEvent {
+}
--- src/main/java/me/samuel/vyma/pluginInterface/VPluginInterface.java
+++ src/main/java/me/samuel/vyma/pluginInterface/VPluginInterface.java
@@ -1,5 +1,39 @@
 package me.samuel.vyma.pluginInterface;
 
-public interface VPluginInterface {
+public abstract class VPluginInterface {
 
+    private String name;
+
+    private double version;
+
+    public VPluginInterface(String name, double version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    public void load() {
+        onLoad();
+        onPostLoad();
+    }
+
+    public void unload() {
+        onUnload();
+        onPostUnload();
+    }
+
+    public abstract void onLoad();
+
+    public abstract void onPostLoad();
+
+    public abstract void onUnload();
+
+    public abstract void onPostUnload();
+
+    public String getName() {
+        return name;
+    }
+
+    public double getVersion() {
+        return version;
+    }
 }
